9dff8030db25c2e5f7803b6199de26cd11fffb7d
Mursalin <mursalin2311@gmail.com>
2024-08-24 14:21:13 +0600

    routes implemented to the app.ts

--- src/app.ts
+++ src/app.ts
@@ -4,12 +4,15 @@ import cookieParser from "cookie-parser";
 import { UserRoutes } from "./app/modules/user/user.routes";
 import { AuthRoutes } from "./app/modules/auth/auth.routes";
 import { errorHandler } from "./app/middlewares/ErrorHandler";
+import { UserExcRoute } from "./app/modules/Conversation/users.routes";
+import { MassageRoute } from "./app/modules/Conversation/messages.routes";
+import { ConversationRoute } from "./app/modules/Conversation/conversation.routes";
 
 const app = express();
 
 // Parsers
-app.use(express.json()); // JSON parser
-app.use(cookieParser()); // Cookie parser
+app.use(express.json()); 
+app.use(cookieParser()); 
 
 // CORS setup
 app.use(
@@ -22,6 +25,9 @@ app.use(
 // Application routes:
 app.use("/api/v2/user", UserRoutes); // Users
 app.use("/auth", AuthRoutes);
+app.use("/api", UserExcRoute);
+app.use("/api", MassageRoute);
+app.use("/api", ConversationRoute);
 
 app.use(errorHandler);
 
