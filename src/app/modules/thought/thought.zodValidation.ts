895b293d7188cc96d876cca8782b19e2f2f0f7bf
Mursalin <mursalin2311@gmail.com>
2024-09-09 03:34:58 +0600

    goodnight

--- src/app/modules/thought/thought.zodValidation.ts
+++ src/app/modules/thought/thought.zodValidation.ts
@@ -4,7 +4,7 @@ export const thoughtValidationSchema = z.object({
   text: z
     .string()
     .min(1, "Text is required")
-    .max(30, "Text cannot exceed 30 characters"),
+    .max(70, "Text cannot exceed 70 characters"),
   name: z.string().min(1, "Name is required"),
   userId: z.string().min(1, "User ID is required"),
   createdAt: z.date().optional(),
